ad4d04113da391701fd5d9dac4044634ea3d2fdd
Elias <localdomain>
2021-04-19 18:44:43 +0300

    added sync progress callback

--- bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/IRuuviGattListener.kt
+++ bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/IRuuviGattListener.kt
@@ -5,4 +5,5 @@ interface IRuuviGattListener {
     fun deviceInfo(model: String, fw: String, canReadLogs: Boolean)
     fun dataReady(data: List<LogReading>)
     fun heartbeat(raw: String)
+    fun syncProgress(syncedDataPoints: Int)
 }
