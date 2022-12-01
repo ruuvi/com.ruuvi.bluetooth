41f0d9d905365e6ea9aac657ce0debd486219350
Denis Andreev <andreev.denis@gmail.com>
2022-12-01 17:35:27 +0300

    Added serial number to device info response

--- bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/IRuuviGattListener.kt
+++ bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/IRuuviGattListener.kt
@@ -2,7 +2,7 @@ package com.ruuvi.station.bluetooth
 
 interface IRuuviGattListener {
     fun connected(state: Boolean)
-    fun deviceInfo(model: String, fw: String, canReadLogs: Boolean)
+    fun deviceInfo(model: String, fw: String, canReadLogs: Boolean, serialNumber: String? = null)
     fun dataReady(data: List<LogReading>)
     fun heartbeat(raw: String)
     fun syncProgress(syncedDataPoints: Int)
